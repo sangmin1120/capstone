591d464ac0c8ff6ec12466d2fd50e2fbf4ff90e2
sangmin1120 <sangmin8276@gmail.com>
2025-03-20 16:15:24 +0900

    comment 부분 반환값, 에러처리 정리

--- src/main/java/smu/capstone/object/comment/controller/CommentController.java
+++ src/main/java/smu/capstone/object/comment/controller/CommentController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import smu.capstone.common.response.BaseResponse;
 import smu.capstone.object.comment.dto.CommentRequestDto;
 import smu.capstone.object.comment.dto.CommentResponseDto;
 import smu.capstone.object.comment.service.CommentService;
@@ -21,27 +22,27 @@ public class CommentController {
 
     //  댓글 작성
     @PostMapping
-    public ResponseEntity<CommentResponseDto> addComment(@PathVariable("boardId") Long boardId,
-                                                         @RequestBody CommentRequestDto requestDto, HttpServletRequest request) {
+    public BaseResponse<CommentResponseDto> addComment(@PathVariable("boardId") Long boardId,
+                                                       @RequestBody CommentRequestDto requestDto, HttpServletRequest request) {
 
         Long userId = infoService.getCurrentUser(request).getId();
         CommentResponseDto response = commentService.addComment(boardId, userId, requestDto);
-        return ResponseEntity.ok(response);
+        return BaseResponse.ok(response);
     }
 
     //  특정 게시글의 모든 댓글 조회
     @GetMapping
-    public ResponseEntity<List<CommentResponseDto>> getComments(@PathVariable("boardId") Long boardId) {
+    public BaseResponse<List<CommentResponseDto>> getComments(@PathVariable("boardId") Long boardId) {
         List<CommentResponseDto> response = commentService.getComments(boardId);
-        return ResponseEntity.ok(response);
+        return BaseResponse.ok(response);
     }
 
     //  댓글 삭제
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<String> deleteComment(@PathVariable("boardId") Long boardId, @PathVariable("commentId") Long commentId
+    public BaseResponse<String> deleteComment(@PathVariable("boardId") Long boardId, @PathVariable("commentId") Long commentId
     , HttpServletRequest request) {
         Long userId = infoService.getCurrentUser(request).getId();
         commentService.deleteComment(commentId, userId);
-        return ResponseEntity.ok("댓글이 삭제되었습니다.");
+        return BaseResponse.ok("댓글이 삭제되었습니다.");
     }
 }
