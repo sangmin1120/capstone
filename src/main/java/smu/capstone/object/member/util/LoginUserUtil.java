cc1a82c64928b8dcc7425ac5a03ac4dfebe34816
sangmin1120 <sangmin8276@gmail.com>
2025-03-23 19:21:48 +0900

    Log userId 가져오는 코드 추가

---
+++ src/main/java/smu/capstone/object/member/util/LoginUserUtil.java
@@ -0,0 +1,16 @@
+package smu.capstone.object.member.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@UtilityClass
+public class LoginUserUtil {
+    public Long getLoginMemberId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.isAuthenticated()) {
+            return Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getName());
+        }
+        return -1L;
+    }
+}
