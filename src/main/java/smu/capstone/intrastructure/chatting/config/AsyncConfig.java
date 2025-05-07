aed6060db61397fae03e4067214976da44f97605
Yang-sumin0000 <202110947@sangmyung.kr>
2025-05-07 12:36:42 +0900

    1. Async thread reject 시 호출스레드에서 처리하도록 수정 2. Redis SessionManager에서 Duration 삭제

--- src/main/java/smu/capstone/intrastructure/chatting/config/AsyncConfig.java
+++ src/main/java/smu/capstone/intrastructure/chatting/config/AsyncConfig.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import smu.capstone.domain.chat.exception.AsyncExceptionHandler;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @EnableAsync
 @RequiredArgsConstructor
@@ -22,6 +23,7 @@ public class AsyncConfig implements AsyncConfigurer {
         executor.setMaxPoolSize(30);        // 최대 스레드 수
         executor.setQueueCapacity(500);     // 대기열 큐 크기
         executor.setThreadNamePrefix("AsyncExecutor-");     //스레드 prifix 커스텀
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());    //스레드 거절될 시 호출한 곳에서 처리
         executor.initialize();
         return executor;
     }
