60a1fa8bf02c937bc9fb564e16780fc82e370f67
sangmin1120 <sangmin8276@gmail.com>
2025-05-11 09:37:51 +0900

    이메일 알림, 디바이스 알림 추가

--- src/main/java/smu/capstone/domain/schedule/controller/PushTestController.java
+++ src/main/java/smu/capstone/domain/schedule/controller/PushTestController.java
@@ -6,16 +6,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import smu.capstone.domain.fcm.dto.MessageNotification;
+import smu.capstone.domain.fcm.service.FCMService;
 import smu.capstone.domain.member.entity.UserEntity;
 import smu.capstone.domain.member.service.InfoService;
-import smu.capstone.domain.schedule.service.WebPushServiceFcm;
 
 @RestController
 @RequestMapping("/api/test")
 @RequiredArgsConstructor
 public class PushTestController {
 
-    private final WebPushServiceFcm webPushServiceFcm;
+    private final FCMService fcmService;
     private final InfoService infoService;
 
     @PostMapping("/send-push")
@@ -23,7 +24,8 @@ public class PushTestController {
         UserEntity user = infoService.getCurrentUser();
         String token = user.getFcmToken();
         if (token != null && !token.isBlank()) {
-            webPushServiceFcm.sendPush(token, "테스트 알림", "이건 수동으로 전송된 웹 푸시 알림입니다.");
+            fcmService.sendMessage(MessageNotification.of(token, "테스트 알림", "이건 수동으로 전송된 웹 푸시 알림입니다."));
+//            webPushServiceFcm.sendPush(token, "테스트 알림", "이건 수동으로 전송된 웹 푸시 알림입니다.");
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.badRequest().build();
