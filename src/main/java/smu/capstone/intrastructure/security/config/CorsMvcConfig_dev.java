a5e407e530289e09208e49a6ce0ad9ad9b1e11c2
sangmin1120 <sangmin8276@gmail.com>
2025-06-03 14:34:04 +0900

    dev/prod 분리

---
+++ src/main/java/smu/capstone/intrastructure/security/config/CorsMvcConfig_dev.java
@@ -0,0 +1,20 @@
+package smu.capstone.intrastructure.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("dev")
+@Configuration
+public class CorsMvcConfig_dev implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 나중에 수정해야됨
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 필요한 메서드 허용
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
