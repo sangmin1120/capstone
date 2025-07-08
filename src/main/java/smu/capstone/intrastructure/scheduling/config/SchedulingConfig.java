1d220064aad0f05b0eb25eb42735dd64e484b1ca
Yang-sumin0000 <202110947@sangmyung.kr>
2025-07-08 17:27:19 +0900

    스케쥴링 스레드 설정 수정 - 리젝으로 변경

--- src/main/java/smu/capstone/intrastructure/scheduling/config/SchedulingConfig.java
+++ src/main/java/smu/capstone/intrastructure/scheduling/config/SchedulingConfig.java
@@ -6,8 +6,6 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
-import java.util.concurrent.ThreadPoolExecutor;
-
 @EnableScheduling
 @Configuration
 public class SchedulingConfig implements SchedulingConfigurer {
@@ -17,7 +15,6 @@ public class SchedulingConfig implements SchedulingConfigurer {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(5); // 병렬 실행할 스레드 수
         scheduler.setThreadNamePrefix("scheduler-thread-");
-        scheduler.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         taskRegistrar.setTaskScheduler(scheduler);
         scheduler.initialize();
     }
